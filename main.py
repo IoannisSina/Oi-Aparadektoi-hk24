5ea62b4f6e296cf01c8125b06637417de93d763a
Gerasimos Papachronopoulos <58421904+geras-papax@users.noreply.github.com>
2024-04-07 03:21:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+import time
 import descriptor
 
 
 # Create the FastAPI instance
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 # Post url to get description
-@app.post("/description")
+@app.get("/description/{url}")
 async def get_description(url:str):
+    # time.sleep(25)
+    # response = f"This is a description for {url}"
     response = descriptor.get_description(url)
     return response
 
@@ -23,4 +36,6 @@ async def get_description(url:str):
 
 # Starting the server function using uvicorn
 def start():
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+start()
